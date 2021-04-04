5d276076e6748cc129385ef35186b502360753cb
arabuli <arabulibeqa@gmail.com>
2021-04-04 18:25:43 +0200

    improved card design

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ function Book(title, author, pages, read) {
   this.read = read
   this.info = function () {
     if (read == true) {
-      return `${title} by ${author}, ${pages}, read`
+      return `${title} ${author} ${pages} read`
     } else {
-      return `${title} by ${author}, ${pages}, not yet read`
+      return `${title} ${author} ${pages} notread`
     }
   }
 }
@@ -22,14 +22,22 @@ function addBookToLibrary(title, author, pages, read) {
 
 addBookToLibrary("Hobbit", "Tolkien", 333, true);
 addBookToLibrary("Wisgni", "Me", 123, false);
+addBookToLibrary("Hobbit", "Tolkien", 333, true);
+addBookToLibrary("Wisgni", "Me", 123, false);
+addBookToLibrary("Hobbit", "Tolkien", 333, true);
+addBookToLibrary("Wisgni", "Me", 123, false);
+addBookToLibrary("Hobbit", "Tolkien", 333, true);
+addBookToLibrary("Wisgni", "Me", 123, false);
+addBookToLibrary("Hobbit", "Tolkien", 333, true);
+addBookToLibrary("Wisgni", "Me", 123, false);
 
 const bookForm = document.getElementById("add-book-form");
 const addButton = document.getElementById("add-book-button");
-addButton.addEventListener("click", function () {
-  if (bookForm.style.display == "none") {
-    bookForm.style.display = "block";
-  } else {
+addButton.addEventListener("click", function() {
+  if(bookForm.style.display == "block") {
     bookForm.style.display = "none";
+  } else {
+    bookForm.style.display = "block";
   }
 });
 
@@ -45,7 +53,6 @@ function getFormInfo() {
   addBookToLibrary(title, author, pages, read);
 }
 
-
 const submitButton = document.getElementById("submit");
 submitButton.addEventListener("click", function () {
   getFormInfo();
@@ -53,18 +60,31 @@ submitButton.addEventListener("click", function () {
   displayBooks();
 });
 
-
-
 function displayBooks() {
   const selectBookPlace = document.getElementById("book-container");
   selectBookPlace.innerHTML = "";
   for (let i = 0; i < myLibrary.length; i++) {
+    let bookTitle = myLibrary[i].info().split(" ")[0];
+    let bookAuthor = myLibrary[i].info().split(" ")[1];
+    let bookPages = myLibrary[i].info().split(" ")[2];
+    let bookStatus = myLibrary[i].info().split(" ")[3];
     let book = document.createElement("div");
-    book.textContent = myLibrary[i].info();
+    let title = document.createElement("p");
+    let author = document.createElement("p");
+    let pages = document.createElement("p");
+    let read = document.createElement("p");
+    title.textContent = `Title: ${bookTitle}`;
+    author.textContent = `Author: ${bookAuthor}`;
+    pages.textContent = `Pages: ${bookPages}`;
+    read.textContent = `Status: ${bookStatus}`;
+    book.appendChild(title);
+    book.appendChild(author);
+    book.appendChild(pages);
+    book.appendChild(read);
     book.classList.add("book");
     selectBookPlace.appendChild(book);
     let removeButton = document.createElement("button");
-    removeButton.textContent = "Remove Book";
+    removeButton.textContent = "x";
     removeButton.classList.add("remove-button");
     book.appendChild(removeButton);
     removeButton.addEventListener("click", function() {
